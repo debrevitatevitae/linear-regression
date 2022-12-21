7c5aebbf045c2fd6426f57912a36b0ee431adb32
Giorgio Tosti Balducci <g.b.l.tostibalducci@tudelft.nl>
2022-12-21 10:45:35 +0100

    lr-cpu-act.py: add contents

---
+++ lr-cpu-act.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+plt.rcParams['font.size'] = 14
+
+from project_directories import raw_data_dir
+
+
+if __name__ == '__main__':
+
+    # Load or download the dataset as a dataframe
+    df_url = 'https://huggingface.co/datasets/inria-soda/tabular-benchmark/raw/main/reg_num/cpu_act.csv'
+
+    if os.path.isfile(raw_data_dir+'cpu_act.csv'):
+        df = pd.read_csv(raw_data_dir+'cpu_act.csv', index_col=0)
+    else:
+        df = pd.read_csv(df_url, index_col=0)
+        df.to_csv(raw_data_dir+'cpu_act.csv', index=True)
+
+    # print(df)
+
+    # Obtain data matrix and values
+    data = df.to_numpy(dtype=np.float32)
+    # print(data)
+    A = data[:, :-1]
+    b = data[:, -1].reshape(-1, 1)
+
+    # Prepend a column of ones to A to act as a bias
+    m, n = A.shape
+    A = np.concatenate((np.ones((m, 1), dtype=np.float32), A), axis=1)
+    # print(A)
+
+    # Solve Ax = b using the SVD to compute the pseud-inverse
+    U, Sig, VT = np.linalg.svd(A, full_matrices=False)
+    x = VT.T @ np.linalg.inv(np.diag(Sig)) @ U.T @ b
+
+    # Plot the actual cpu times in user mode vs the ones predicted with linear regression
+    fig, ax = plt.subplots()
+
+    ax.plot(b, 'k-', lw=2, label='True cpu user times')
+    ax.plot(A@x, 'b--', lw=1.5, ms=6, alpha=.8, label='Regression values')
+    ax.set(xlabel='user', ylabel='cpu user time')
+    ax.legend()
+    plt.show()
